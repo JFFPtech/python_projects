7a7761e39ea20e2b36981268963b221d7738f4a5
JFFPtech <arrozconsalchichas1229@gmail.com>
2024-02-03 12:11:27 -0400

    simple funcs and returns

---
+++ pythonbasics/functions.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Functions are a block of code that only runs when it is called. 
+
+def hello(firstname, lastname, age):
+    print("Hello " +firstname+" "+lastname)
+    print("You are "+str(age)+" years old.")
+    print("Have a great day!")
+
+hello("Javi", "Gomez", 21)
---
+++ pythonbasics/returns.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Returns are values that a function returns to the caller.
+
+def multiply(number1, number2):
+    return number1 * number2
+
+x = multiply(3, 5)
+
+print(x)
